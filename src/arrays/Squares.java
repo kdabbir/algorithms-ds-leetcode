9d00e6b4a07962aff50ade9fcebf07241d9b7419
Kousthubha <d.kousthubhakumar@gmail.com>
2020-06-10 23:25:33 +0530

    added array of squares of each number

--- src/arrays/Squares.java
+++ src/arrays/Squares.java
@@ -1,10 +1,10 @@
-package arrays;
+package app;
 import java.util.Arrays;
 // Problem: Given a sorted array in non-decreasing order, return an array of squares of each number, also in non-decreasing order. For example:
 // Sample input: [-4,-2,-1,0,3,5] 
 // Sample output: [0,1,4,9,16,25]
 
-public class Squares {
+public class Arrays_Squares {
     public static void main(String[] args) throws Exception {
         System.out.println(Arrays.toString(squares(new int[]{-4,-2,-1,0,3,5})));
     }
