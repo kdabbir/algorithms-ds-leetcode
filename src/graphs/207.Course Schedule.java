f6c54f26e48a3dbecffd4621c433d5542a2caa58
Kousthubha Kumar <d.kousthubhakumar@gmail.com>
2020-07-19 19:14:46 +0530

    added topological sort algorithm

---
+++ src/graphs/207.Course Schedule.java
@@ -0,0 +1,81 @@
+// 207. Course Schedule
+// Medium
+
+// There are a total of numCourses courses you have to take, labeled from 0 to numCourses-1.
+
+// Some courses may have prerequisites, for example to take course 0 you have to first take course 1, which is expressed as a pair: [0,1]
+
+// Given the total number of courses and a list of prerequisite pairs, is it possible for you to finish all courses?
+
+// Example 1:
+
+// Input: numCourses = 2, prerequisites = [[1,0]]
+// Output: true
+// Explanation: There are a total of 2 courses to take. 
+//              To take course 1 you should have finished course 0. So it is possible.
+// Example 2:
+
+// Input: numCourses = 2, prerequisites = [[1,0],[0,1]]
+// Output: false
+// Explanation: There are a total of 2 courses to take. 
+//              To take course 1 you should have finished course 0, and to take course 0 you should
+//              also have finished course 1. So it is impossible.
+ 
+
+// Constraints:
+
+// The input prerequisites is a graph represented by a list of edges, not adjacency matrices. Read more about how a graph is represented.
+// You may assume that there are no duplicate edges in the input prerequisites.
+// 1 <= numCourses <= 10^5
+
+
+// Using topological sort.
+
+class GNode{
+    public Integer inDegree = 0;
+    public List<Integer> outNodes = new LinkedList<Integer>();
+}
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        if(prerequisites == null || prerequisites.length == 0 || numCourses == 0 || prerequisites[0].length == 0) 
+            return true;
+        HashMap<Integer, GNode> preReqOrder = new HashMap<>();
+        for(int[] row: prerequisites) {
+            GNode prevNode = createGNode(preReqOrder, row[1]);
+            GNode nextNode = createGNode(preReqOrder, row[0]);
+            prevNode.outNodes.add(row[0]);
+            nextNode.inDegree++;
+        }
+        
+        LinkedList<Integer> zeroReqNodes = new LinkedList<>();
+        for (Integer courseNum : preReqOrder.keySet()) {
+            if(preReqOrder.get(courseNum).inDegree == 0)
+                zeroReqNodes.add(courseNum);
+        }
+        
+        Integer removedNodes = 0;
+        while(zeroReqNodes.size() > 0){
+            Integer curr = zeroReqNodes.pop();
+            for(Integer currNumCourse: preReqOrder.get(curr).outNodes){
+                GNode outNodes = preReqOrder.get(currNumCourse);
+                outNodes.inDegree--;
+                removedNodes++;
+                if(outNodes.inDegree == 0)
+                    zeroReqNodes.add(currNumCourse);
+            }
+        }
+        if(removedNodes == prerequisites.length)
+            return true;
+        else
+            return false;
+    }
+    
+    public GNode createGNode(HashMap<Integer,GNode> hashList, Integer courseNumber){
+        if(hashList.containsKey(courseNumber)){
+            return hashList.get(courseNumber);
+        }
+        GNode gnode = new GNode();
+        hashList.put(courseNumber, gnode);
+        return gnode;
+    }
+}
