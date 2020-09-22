91a180ff04df820259bb451661c7fd159eb50ce8
Kousthubha <d.kousthubhakumar@gmail.com>
2020-09-22 17:08:48 +0530

    Update 11. Container With Most Water.java

--- src/arrays/11. Container With Most Water.java
+++ src/arrays/11. Container With Most Water.java
@@ -30,6 +30,9 @@ class Solution {
 }
 
 // One pass O(N) solution
+
+// Initially we consider the area constituting the exterior most lines. Now, to maximize the area, we need to consider the area between the lines of larger lengths. If we try to move the pointer at the longer line inwards, we won't gain any increase in area, since it is limited by the shorter line. But moving the shorter line's pointer could turn out to be beneficial, as per the same argument, despite the reduction in the width. This is done since a relatively longer line obtained by moving the shorter line's pointer might overcome the reduction in area caused by the width reduction.
+
 class Solution {
     public int maxArea(int[] height) {
         int i = 0;
@@ -42,7 +45,7 @@ class Solution {
             // Move which ever height is the least of both, since we need higher values for max area.
             if(height[i] < height[j]){
                 i++;
-            } else {
+            } else {x
                 j--;
             }
             
