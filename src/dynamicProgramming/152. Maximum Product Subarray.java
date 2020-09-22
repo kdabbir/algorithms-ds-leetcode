fbdb15fc2f998aa05933eb14a9e07b5bcb405adb
Kousthubha <d.kousthubhakumar@gmail.com>
2020-09-22 16:09:34 +0530

    Create 152. Maximum Product Subarray.java

---
+++ src/dynamicProgramming/152. Maximum Product Subarray.java
@@ -0,0 +1,44 @@
+// 152. Maximum Product Subarray:  https://leetcode.com/problems/maximum-product-subarray/
+// Medium
+// Given an integer array nums, find the contiguous subarray within an array (containing at least one number) which has the largest product.
+
+// Example 1:
+
+// Input: [2,3,-2,4]
+// Output: 6
+// Explanation: [2,3] has the largest product 6.
+// Example 2:
+
+// Input: [-2,0,-1]
+// Output: 0
+// Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
+
+class Solution {
+    public int maxProduct(int[] nums) {
+        if(nums == null || nums.length == 0) return 0;
+        int minSumSoFar = nums[0], maxSumSoFar = nums[0], maxProd = nums[0];
+        for(int i = 1; i < nums.length; i++) {
+           int currMax = Math.max(nums[i], Math.max(maxSumSoFar * nums[i], minSumSoFar * nums[i]));
+            minSumSoFar = Math.min(nums[i], Math.min(maxSumSoFar * nums[i], minSumSoFar * nums[i]));
+            maxSumSoFar = currMax;
+            maxProd = Math.max(maxProd, maxSumSoFar);   
+        }
+        return maxProd;
+    }
+}   
+
+// Time: O(N)
+// Space: O(1)
+
+// Approach:
+
+// max_so_far is updated by taking the maximum value among:
+
+// Current number.
+// This value will be picked if the accumulated product has been really bad (even compared to the current number). This can happen when the current number has a preceding zero (e.g. [0,4]) or is preceded by a single negative number (e.g. [-3,5]).
+// Product of last max_so_far and current number.
+// This value will be picked if the accumulated product has been steadily increasing (all positive numbers).
+// Product of last min_so_far and current number.
+// This value will be picked if the current number is a negative number and the combo chain has been disrupted by a single negative number before (In a sense, this value is like an antidote to an already poisoned combo chain).
+// min_so_far is updated in using the same three numbers except that we are taking minimum among the above three numbers.
+
