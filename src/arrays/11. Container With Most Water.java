ab81e9d27a8e88e87a4a3b64033d53929746c126
Kousthubha <d.kousthubhakumar@gmail.com>
2020-09-22 17:12:22 +0530

    Update 11. Container With Most Water.java

--- src/arrays/11. Container With Most Water.java
+++ src/arrays/11. Container With Most Water.java
@@ -31,8 +31,6 @@ class Solution {
 
 // One pass O(N) solution
 
-// Initially we consider the area constituting the exterior most lines. Now, to maximize the area, we need to consider the area between the lines of larger lengths. If we try to move the pointer at the longer line inwards, we won't gain any increase in area, since it is limited by the shorter line. But moving the shorter line's pointer could turn out to be beneficial, as per the same argument, despite the reduction in the width. This is done since a relatively longer line obtained by moving the shorter line's pointer might overcome the reduction in area caused by the width reduction.
-
 class Solution {
     public int maxArea(int[] height) {
         int i = 0;
@@ -53,3 +51,20 @@ class Solution {
         return max;
     }
 }
+
+// Complexity Analysis
+
+// Time complexity : O(n)O(n). Single pass.
+
+// Space complexity : O(1)O(1). Constant space is used.
+
+// Initially we consider the area constituting the exterior most lines. Now, to maximize the area, we need to consider the area between the lines of larger lengths. If we try to move the pointer at the longer line inwards, we won't gain any increase in area, since it is limited by the shorter line. But moving the shorter line's pointer could turn out to be beneficial, as per the same argument, despite the reduction in the width. This is done since a relatively longer line obtained by moving the shorter line's pointer might overcome the reduction in area caused by the width reduction.
+
+
+// Explanation for i++ when arr[i] < arr[j]
+
+// You have two heights H_left and H_right, and H_right < H_left, then we know we have two choices, we want to move one of them. If we move the larger one, we cannot increase the height for the simple reason that we are always limited by the shortest, and we would be decreasing j-i, the width as well.
+
+// To clarify: let's say we kept the shortest forever, what would happen? Well, j-i would decrease, and either we come across a taller block, which doesn't matter because our shorter one we kept only mattered, or we find a shorter one, in which case that one matters.
+
+// Either way we end up with a smaller area, so we must move the shorter one because moving the larger one cannot give an increase in area.
