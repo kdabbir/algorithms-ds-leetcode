565226542b0100e65844e0de217ddd5858d8b615
Kousthubha <d.kousthubhakumar@gmail.com>
2020-09-27 18:14:20 +0530

    Update 268. Missing Number.java

--- src/bitManipulation/268. Missing Number.java
+++ src/bitManipulation/268. Missing Number.java
@@ -88,3 +88,39 @@ class Solution {
         return -1;
     }
 }
+
+// Sorting
+
+// First, we sort nums. Then, we check the two special cases that can be handled in constant time - ensuring that 0 is at the beginning and that nn is at the end. Given that those assumptions hold, the missing number must somewhere between (but not including) 0 and nn. To find it, we ensure that the number we expect to be at each index is indeed there. Because we handled the edge cases, this is simply the previous number plus 1. Thus, as soon as we find an unexpected number, we can simply return the expected number.
+
+class Solution {
+    public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+
+        // Ensure that n is at the last index
+        if (nums[nums.length-1] != nums.length) {
+            return nums.length;
+        }
+        // Ensure that 0 is at the first index
+        else if (nums[0] != 0) {
+            return 0;
+        }
+
+        // If we get here, then the missing number is on the range (0, n)
+        for (int i = 1; i < nums.length; i++) {
+            int expectedNum = nums[i-1] + 1;
+            if (nums[i] != expectedNum) {
+                return expectedNum;
+            }
+        }
+
+        // Array was not missing any numbers
+        return -1;
+    }
+}
+
+// Time complexity : O(nlgn)
+
+// The only elements of the algorithm that have asymptotically nonconstant time complexity are the main for loop (which runs in \mathcal{O}(n)O(n) time), and the sort invocation (which runs in \mathcal{O}(nlgn)O(nlgn) time for Python and Java). Therefore, the runtime is dominated by sort, and the entire runtime is \mathcal{O}(nlgn)O(nlgn).
+
+// Space complexity : O(1) or O(n)
