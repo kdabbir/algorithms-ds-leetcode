f2e2ded97abdf014ecbf97cd27932d48267769fc
Kousthubha <d.kousthubhakumar@gmail.com>
2020-09-29 23:57:02 +0530

    Create 953. Verifying an Alien Dictionary.java

---
+++ src/arrays/953. Verifying an Alien Dictionary.java
@@ -0,0 +1,66 @@
+// 953. Verifying an Alien Dictionary : https://leetcode.com/problems/verifying-an-alien-dictionary/
+// Easy
+// In an alien language, surprisingly they also use english lowercase letters, but possibly in a different order. The order of the alphabet is some permutation of lowercase letters.
+
+// Given a sequence of words written in the alien language, and the order of the alphabet, return true if and only if the given words are sorted lexicographicaly in this alien language.
+
+ 
+
+// Example 1:
+
+// Input: words = ["hello","leetcode"], order = "hlabcdefgijkmnopqrstuvwxyz"
+// Output: true
+// Explanation: As 'h' comes before 'l' in this language, then the sequence is sorted.
+// Example 2:
+
+// Input: words = ["word","world","row"], order = "worldabcefghijkmnpqstuvxyz"
+// Output: false
+// Explanation: As 'd' comes after 'l' in this language, then words[0] > words[1], hence the sequence is unsorted.
+// Example 3:
+
+// Input: words = ["apple","app"], order = "abcdefghijklmnopqrstuvwxyz"
+// Output: false
+// Explanation: The first three characters "app" match, and the second string is shorter (in size.) According to lexicographical rules "apple" > "app", because 'l' > '∅', where '∅' is defined as the blank character which is less than any other character (More info).
+ 
+
+// Constraints:
+
+// 1 <= words.length <= 100
+// 1 <= words[i].length <= 20
+// order.length == 26
+// All characters in words[i] and order are English lowercase letters.
+
+// Approach 1: Check Adjacent Words
+
+class Solution {
+    public boolean isAlienSorted(String[] words, String order) {
+        Map<Character, Integer> map = new HashMap<>();
+        int init = 0;
+        for(char character: order.toCharArray()) {
+            map.put(character, init);
+            init++;
+        }
+        outer:
+        for(int i = 0; i < words.length - 1; i++) {
+            String curr = words[i];
+            String adj = words[i + 1];
+            for(int j = 0; j < Math.min(curr.length(), adj.length()); j++) {
+                if(curr.charAt(j) != adj.charAt(j)) {
+                        if(map.get(curr.charAt(j)) > map.get(adj.charAt(j))) {
+                            return false;
+                        }
+                        continue outer;
+                }
+            }
+            if (curr.length() > adj.length()) return false;  
+        }
+        return true;
+        
+    }
+}
+
+// Complexity Analysis
+
+// Time Complexity: O(C), where C is the total content of words.
+
+// Space Complexity: O(1).
