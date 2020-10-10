f7af1ae1233ea422547c3b63bc048a31a9a2806c
Kousthubha <d.kousthubhakumar@gmail.com>
2020-10-10 18:26:34 +0530

    Create 31. Next Permutation.java

---
+++ src/arrays/31. Next Permutation.java
@@ -0,0 +1,6 @@
+// 31. Next Permutation
+
+// Using Next lexicographical permutation algorithm : https://www.nayuki.io/page/next-lexicographical-permutation-algorithm
+// https://leetcode.com/problems/next-permutation/discuss/13994/Readable-code-without-confusing-ij-and-with-explanation
+
+
