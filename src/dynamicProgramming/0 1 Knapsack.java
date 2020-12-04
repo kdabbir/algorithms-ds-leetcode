2f695fb58bcf48ced79f602f6a315fbdb36ea7fb
Kousthubha <d.kousthubhakumar@gmail.com>
2020-12-04 22:50:24 +0530

    Create 0 1 Knapsack.java

---
+++ src/dynamicProgramming/0 1 Knapsack.java
@@ -0,0 +1,50 @@
+// 0 - 1 Knapsack Problem 
+// Hard
+
+// You are given weights and values of N items, put these items in a knapsack of capacity W to get the maximum total value in the knapsack. Note that we have only one quantity of each item.
+// In other words, given two integer arrays val[0..N-1] and wt[0..N-1] which represent values and weights associated with N items respectively. Also given an integer W which represents knapsack capacity, find out the maximum value subset of val[] such that sum of the weights of this subset is smaller than or equal to W. You cannot break an item, either pick the complete item, or don’t pick it (0-1 property).
+
+// Example 1:
+
+// Input:
+// N = 3
+// W = 4
+// values[] = {1,2,3}
+// weight[] = {4,5,1}
+// Output: 3
+// Example 2:
+
+// Input:
+// N = 3
+// W = 3
+// values[] = {1,2,3}
+// weight[] = {4,5,6}
+// Output: 0
+
+// Explanation: https://www.youtube.com/watch?v=8LusJS5-AGo&t=1s
+
+    class Knapsack 
+    { 
+        // Returns the maximum value that can be put in a knapsack of capacity W 
+        static int knapSack(int W, int wt[], int val[], int n) 
+        { 
+            int[][] dpMemo = new int[n + 1][W + 1];
+            
+            for (int row = 0; row < dpMemo.length; row++) {
+                for (int col = 0; col < dpMemo[row].length; col++) {
+                    if(row == 0 || col == 0) dpMemo[row][col] = 0;
+                    
+                    else if(wt[row - 1] <= col) {
+                        dpMemo[row][col] = Math.max(val[row - 1] + dpMemo[row - 1][col - wt[row-1]],
+                                                    dpMemo[row - 1][col] );
+                    } else {
+                        dpMemo[row][col] = dpMemo[row - 1][col];
+                    }
+                }
+            }
+            return dpMemo[wt.length][W];
+        } 
+    }
+    
+    
+    
