a054a8df7dc862521bdc95c893d5a37c6a03382a
Kousthubha <d.kousthubhakumar@gmail.com>
2020-12-26 18:30:59 +0530

    Create 64. Minimum Path Sum.java

---
+++ src/matrix/64. Minimum Path Sum.java
@@ -0,0 +1,44 @@
+// 64. Minimum Path Sum: https://leetcode.com/problems/minimum-path-sum/
+// Medium
+
+// Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right, which minimizes the sum of all numbers along its path.
+
+// Note: You can only move either down or right at any point in time.
+
+ 
+
+// Example 1:
+
+
+// Input: grid = [[1,3,1],[1,5,1],[4,2,1]]
+// Output: 7
+// Explanation: Because the path 1 → 3 → 1 → 1 → 1 minimizes the sum.
+// Example 2:
+
+// Input: grid = [[1,2,3],[4,5,6]]
+// Output: 12
+ 
+
+// Constraints:
+
+// m == grid.length
+// n == grid[i].length
+// 1 <= m, n <= 200
+// 0 <= grid[i][j] <= 100
+
+
+class Solution {
+    public int minPathSum(int[][] grid) {
+        int rowLen = grid.length, colLen = grid[0].length;
+        for(int i = 1; i < rowLen; i++) grid[i][0] += grid[i-1][0];
+        
+        for(int j = 1; j < colLen; j++) grid[0][j] += grid[0][j-1];
+        
+        for(int row = 1; row < rowLen; row++) {
+            for(int col = 1; col < colLen; col++) {
+                grid[row][col] += Math.min(grid[row - 1][col], grid[row][col-1]);
+            }
+        }
+        return grid[rowLen - 1][colLen - 1];
+    }
+}
