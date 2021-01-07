8b1eff2272385c27b233a9a52299c91328d96cbd
Kousthubha <d.kousthubhakumar@gmail.com>
2021-01-07 19:36:20 +0530

    Update 896. Monotonic Array.java

--- src/arrays/896. Monotonic Array.java
+++ src/arrays/896. Monotonic Array.java
@@ -47,4 +47,58 @@ class Solution {
         }
         return isDecreasing || isIncreasing;
     }
+
+    // Another way
+    // 3
+    // class Program {
+    // 4
+    //   // O(n) time | O(1) space - where n is the length of the array
+    // 5
+    //   public static boolean isMonotonic(int[] array) {
+    // 6
+    //     if (array.length <= 2) return true;
+    // 7
+    // ​
+    // 8
+    //     var direction = array[1] - array[0];
+    // 9
+    //     for (int i = 2; i < array.length; i++) {
+    // 10
+    //       if (direction == 0) {
+    // 11
+    //         direction = array[i] - array[i - 1];
+    // 12
+    //         continue;
+    // 13
+    //       }
+    // 14
+    // ​
+    // 15
+    //       if (breaksDirection(direction, array[i - 1], array[i])) {
+    // 16
+    //         return false;
+    // 17
+    //       }
+    // 18
+    //     }
+    // 19
+    //     return true;
+    // 20
+    //   }
+    // 21
+    // ​
+    // 22
+    //   public static boolean breaksDirection(int direction, int previous, int current) {
+    // 23
+    //     var difference = current - previous;
+    // 24
+    //     if (direction > 0) return difference < 0;
+    // 25
+    //     return difference > 0;
+    // 26
+    //   }
+    // 27
+    // }
+    // 28
 }
+
