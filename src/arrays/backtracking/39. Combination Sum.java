5b9ad4dd6b46427dae3e7d575e5538895322e7ed
Kousthubha <d.kousthubhakumar@gmail.com>
2021-03-01 19:33:28 +0530

    Create 39. Combination Sum.java

---
+++ src/arrays/backtracking/39. Combination Sum.java
@@ -0,0 +1,90 @@
+// 39. Combination Sum: https://leetcode.com/problems/combination-sum/
+// Medium
+// Given an array of distinct integers candidates and a target integer target, return a list of all unique combinations of candidates where the chosen numbers sum to target. You may return the combinations in any order.
+
+// The same number may be chosen from candidates an unlimited number of times. Two combinations are unique if the frequency of at least one of the chosen numbers is different.
+
+// It is guaranteed that the number of unique combinations that sum up to target is less than 150 combinations for the given input.
+
+ 
+
+// Example 1:
+
+// Input: candidates = [2,3,6,7], target = 7
+// Output: [[2,2,3],[7]]
+// Explanation:
+// 2 and 3 are candidates, and 2 + 2 + 3 = 7. Note that 2 can be used multiple times.
+// 7 is a candidate, and 7 = 7.
+// These are the only two combinations.
+// Example 2:
+
+// Input: candidates = [2,3,5], target = 8
+// Output: [[2,2,2,2],[2,3,3],[3,5]]
+// Example 3:
+
+// Input: candidates = [2], target = 1
+// Output: []
+// Example 4:
+
+// Input: candidates = [1], target = 1
+// Output: [[1]]
+// Example 5:
+
+// Input: candidates = [1], target = 2
+// Output: [[1,1]]
+ 
+
+// Constraints:
+
+// 1 <= candidates.length <= 30
+// 1 <= candidates[i] <= 200
+// All elements of candidates are distinct.
+// 1 <= target <= 500
+
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> output = new ArrayList<List<Integer>>();
+        Arrays.sort(candidates);
+        backTrack(new ArrayList<>(), output, 0, target,  candidates);
+        return output;
+    }
+    
+    public void backTrack(ArrayList<Integer> curr, List<List<Integer>> output, int processIdx, int currentRem, int[] candidates) {
+        if(currentRem < 0) return;
+        else if(currentRem == 0) {
+            output.add(new ArrayList<>(curr));
+        } else {
+            for(int idx = processIdx; idx < candidates.length; idx++) {
+                curr.add(candidates[idx]);
+                backTrack(curr, output, idx, currentRem - candidates[idx], candidates);
+                curr.remove(curr.size() - 1);
+            }
+        }
+    }
+}
+
+
+// Generate all combinations (even non-unique) matching upto a number 
+
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> output = new ArrayList<List<Integer>>();
+        Arrays.sort(candidates);
+        backTrack(new ArrayList<>(), output, 0, target, target,  candidates);
+        return output;
+    }
+    
+    public void backTrack(ArrayList<Integer> curr, List<List<Integer>> output, int processIdx, int target, int currentRem, int[] candidates) {
+        if(currentRem < 0) return;
+        else if(currentRem == 0) {
+            output.add(new ArrayList<>(curr));
+        } else {
+            for(int idx = processIdx; idx < candidates.length; idx++) {
+                curr.add(candidates[idx]);
+                backTrack(curr, output, processIdx, target, currentRem - candidates[idx], candidates);
+                curr.remove(curr.size() - 1);
+            }
+        }
+    }
+}
+
