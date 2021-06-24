3148124aa682d7c2310ac605707b234a27500789
Kousthubha <d.kousthubhakumar@gmail.com>
2021-06-24 16:55:38 +0530

    Update 621. Task Scheduler.java

--- src/arrays/greedy/621. Task Scheduler.java
+++ src/arrays/greedy/621. Task Scheduler.java
@@ -41,6 +41,9 @@
 // tasks[i] is upper-case English letter.
 // The integer n is in the range [0, 100].
 
+
+// Greedy approach. We consider max number of placeable slots and create empty slots as per cooldown. Then figure out idle slots
+
 class Solution {
     public int leastInterval(char[] tasks, int n) {
         int[] frequencies = new int[26];
@@ -49,10 +52,10 @@ class Solution {
         }
         Arrays.sort(frequencies);
         int maxFreqTask = frequencies[25];
-        int idleTime = (maxFreqTask - 1) * n; // Max idle time is 1 minus since we dont consider adding a task at end.
+        int idleTime = (maxFreqTask - 1) * n; // Max idle time is 1 minus since we dont consider adding a task at end. Here we consider that maxTask is already added.
 
         for(int currTask = frequencies.length - 2; currTask >=0 && idleTime > 0; currTask--) {
-            if(frequencies[currTask] == maxFreqTask) {  // If there are duplicate maxFreqTask then idleTime should be reduced by 1 since we dont consider adding a task at end.
+            if(frequencies[currTask] == maxFreqTask) {  // If there are duplicate maxFreqTask then idleTime should be reduced by 1 since we dont consider adding a task at end i.e we are grouping with max task value.
                 idleTime -= frequencies[currTask] - 1;
             } else {
                 idleTime -= frequencies[currTask];
@@ -66,3 +69,40 @@ class Solution {
 
 // Time Complexity: O(N)
 // Space: O(N)
+
+// Another optimized greedy algorithm with O(N) space, no sorting, O(1) space.
+
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        int[] frequencies = new int[26];
+        int maxTask = 0, maxTaskCount = 0;
+        for(char task: tasks) {
+            frequencies[task - 'A']++;
+            if(frequencies[task - 'A'] > maxTask) {
+                maxTask = frequencies[task - 'A'] ;
+                maxTaskCount = 1;
+            } else if(frequencies[task - 'A'] == maxTask){
+                maxTaskCount++;
+            }
+        }
+
+        int totalNoOfParts = maxTask - 1;
+        int partLength = n - (maxTaskCount - 1);
+        int totalEmptySlots = totalNoOfParts * partLength;
+        int slotsToFill = tasks.length - (maxTask * maxTaskCount);
+        int idleSlots = Math.max(0, totalEmptySlots - slotsToFill);
+        return tasks.length + idleSlots;
+    }
+}
+// Explanation
+// maxTask = How many times did the max element occur; In AAABBC, max is 3;
+// maxTaskCount = AAABBBCCD, maxTaskCount is 2 because A and B both are max occuring thrice;
+// totalNoOfParts = In AAABBC and n=2, we arrange like - AAA, so ** and **, so 2 parts;
+// partLength or NewN = In AAABBBC and n=2, we arrange like - ABABAB, which is, XXX, so n for us will decrease
+// totalEmptySlots = Empty Slots, which is part length * no of parts
+// *slotsToFill/AvailableTasks = all tasks - (the max parts that we've considered so far);
+// *idleSlots = If empty slots are less, we don't care; if they are more, we just want, how many more?
+// Finally, combination of idles and total number of tasks will give answer for intervals needed.
+
+// Time: O(N)
+// Space: O(1)
