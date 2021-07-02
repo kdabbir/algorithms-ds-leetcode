20b0ecd0b9508e6ade318b5eb521914bcd33b384
Kousthubha <d.kousthubhakumar@gmail.com>
2021-07-02 17:00:45 +0530

    Update 74. Search a 2D Matrix.java

--- src/matrix/74. Search a 2D Matrix.java
+++ src/matrix/74. Search a 2D Matrix.java
@@ -4,7 +4,7 @@
 
 // Integers in each row are sorted from left to right.
 // The first integer of each row is greater than the last integer of the previous row.
- 
+
 
 // Example 1:
 
@@ -16,7 +16,7 @@
 
 // Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
 // Output: false
- 
+
 
 // Constraints:
 
@@ -31,12 +31,12 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         int rows = matrix.length;
-        if(rows == 0) 
+        if(rows == 0)
             return false;
         int cols = matrix[0].length;
         int startIdx = 0, endIdx = rows * cols - 1;
         int midIdx, midElement;
-        
+
         while(startIdx <= endIdx) {
             midIdx = startIdx + (endIdx - startIdx)/2;
             midElement = matrix[midIdx / cols][midIdx % cols];
@@ -46,7 +46,7 @@ class Solution {
             if(midElement == target){
                 return true;
             } else if(midElement > target) {
-                endIdx = midIdx - 1;  
+                endIdx = midIdx - 1;
             } else {
                 startIdx = midIdx + 1;
             }
@@ -55,10 +55,62 @@ class Solution {
     }
 }
 
+// The point is to visualize that 2-D array is stored as 1-D array, i.e. to "flatten" it out (in fact, that's how 2-D arrays are stored in RAM. Google row-major array storage).
+
+// I have listed flattened out index in parentheses next to the array index. Note that the flattened out index runs consequently from 0 to 11 for total of 12 elements.
+
+// For example for matrix[1][2] = 16, the flattened out index is 6. Now, if you do that math, it is very clear. 6/4 = 1 for row, and 6%4 = 2 for the column!
+
+// matrix = [
+//   [1 (0), 3 (1), 5 (2), 7 (3)],
+//   [10(4), 11(5), 16(6), 20(7)],
+//   [23(8), 30(9), 34(10), 50(11)]
+// ]
+
 
 // Time: O(logn(NM))
 // Space: O(1)
 
+// Double binary search.
+
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        // Thinking about binary search on first column.
+        // Then perform binary search on selected row.
+        int rowLen = matrix.length - 1, colLen = matrix[0].length - 1;
+        int start = 0, end = rowLen;
+        while(start < end) {
+            int mid = start + (end - start) / 2;
+            int value = matrix[mid][0];
+            if(value <= target && matrix[mid][colLen] >= target ) {
+                break;
+            } else if (value < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        int matchedRow = (start + end)/ 2;
+        start = 0; end = colLen;
+        while(start < end) {
+            int mid =  start + (end - start) / 2;
+            int value = matrix[matchedRow][mid];
+            if(value == target) {
+                return true;
+            } else if (value < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return matrix[matchedRow][(start + end) / 2] == target;
+
+    }
+}
+
+// Time: O(LogN + LogN) = O(LogN)
+// Space: O(1)
 
 // Naive solution
 
