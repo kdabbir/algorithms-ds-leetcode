97fd663d490a61fb4442cf673ff42181065f7fdc
Kousthubha <d.kousthubhakumar@gmail.com>
2021-07-14 00:39:30 +0530

    Update 222. Count Complete Tree Nodes.java

--- src/trees/heightOfTree/222. Count Complete Tree Nodes.java
+++ src/trees/heightOfTree/222. Count Complete Tree Nodes.java
@@ -82,3 +82,62 @@ class Solution {
 
 // Time: (LogN * LogN)
 // Space: O(1)
+
+// Using binary search
+
+class Solution {
+    // Return tree depth in O(d) time.
+    public int computeTillLastLevelDepth(TreeNode node) {
+      int currDepth = 0;
+      while (node != null && node.left != null) { // Here if we just use node != null, it will check entire node depth
+        node = node.left;
+        currDepth++;
+      }
+      return currDepth;
+    }
+
+        // Last level nodes are enumerated from 0 to 2**d - 1 (left -> right).
+        // Return True if last level node idx exists.
+        // Binary search with O(d) complexity.
+      public boolean elementFound(int idx, int depth, TreeNode node) {
+          int start = 0, end = (int) Math.pow(2, depth) - 1;
+          for(int currDepth = 0;currDepth < depth; currDepth++) {
+              int mid = start + (end - start)/2;
+              if(idx <= mid) {
+                  end = mid;
+                  node = node.left;
+              } else {
+                  start = mid + 1;
+                  node = node.right;
+              }
+          }
+          return node != null;
+      }
+
+    public int countNodes(TreeNode root) {
+      // if the tree is empty
+      if (root == null) return 0;
+
+      int tillLastLevelDepth = computeTillLastLevelDepth(root); // Compute depth for N-1 levels
+      // if the tree contains 1 node
+      if (tillLastLevelDepth == 0) return 1;
+
+        // Last level nodes are enumerated from 0 to 2**d - 1 (left -> right).
+        // Perform binary search to check how many nodes exist.
+      int start = 1, end = (int) Math.pow(2, tillLastLevelDepth) - 1;
+      while(start <= end) {
+          int mid = start + (end - start)/2;
+          if(elementFound(mid, tillLastLevelDepth, root)) {
+              start = mid + 1;
+          } else {
+              end = mid - 1;
+          }
+      }
+        // The tree contains 2**d - 1 nodes on the first (d - 1) levels
+        // and left nodes on the last level.
+      return (int) Math.pow(2, tillLastLevelDepth) - 1  + start;
+    }
+  }
+
+  // Time: O(LogN * LogN)
+  // Space: O(1)
