8f1fc0dd7e42e58b7abaaab4978721dd259a1d00
Kousthubha <d.kousthubhakumar@gmail.com>
2021-07-14 00:37:21 +0530

    Update 209. Minimum Size Subarray Sum.java

--- src/arrays/slidingWindow/209. Minimum Size Subarray Sum.java
+++ src/arrays/slidingWindow/209. Minimum Size Subarray Sum.java
@@ -30,6 +30,41 @@
 // Follow up: If you have figured out the O(n) solution, try coding another solution of which the time complexity is O(n log(n)).
 
 
+// Using treemap.
+
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+        int minLength = Integer.MAX_VALUE, sum = 0;
+        TreeMap<Integer, Integer> sumMap = new TreeMap<>();
+        sumMap.put(0, -1);
+        for(int idx = 0; idx < nums.length; idx++) {
+            sum += nums[idx];
+            if(sumMap.floorKey(sum - target) != null) {
+                int currLength = idx - sumMap.get(sumMap.floorKey(sum - target));
+                minLength = Math.min(minLength, currLength);
+            }
+            sumMap.put(sum, idx);
+        }
+
+        return minLength == Integer.MAX_VALUE ?  0 : minLength;
+   }
+}
+
+// Based on the idea in 325. Maximum Size Subarray Sum Equals K, we can use a tree map for solving this problem.
+
+// Modification:
+
+// When we have duplicate prefix sums, we update the index. It is because in this problem we want the minimum size. However, since the problem statement says there are all positive integers, there won't be duplicate prefix sums.
+
+//   x      s
+// -----> ------>
+// ------------->
+//      sum
+// Also, in this problem we want the sum greater than or equal to s, so we cannot use hash map. Instead, we use a tree map. In the example above, when we have a prefix sum sum, we check if there is an x such that sum - x >= s. In other words, we want x <= sum - s. It is to find the greatest x that satisfies the equation. This can be achieved by using floorKey(K) in TreeMap.
+
+// Time: O(NLogN)
+// Space: O(N)
+
 // Two pointers solution.
 
 class Solution {
