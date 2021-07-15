3a2160555d5d43f61c3cd95e7a303d3beb762574
Kousthubha <d.kousthubhakumar@gmail.com>
2021-07-15 19:38:51 +0530

    Update 278. First Bad Version.java

--- src/arrays/binarySearch/278. First Bad Version.java
+++ src/arrays/binarySearch/278. First Bad Version.java
@@ -48,3 +48,19 @@
     }
     // Time: Log(N)
     // Space: O(1)
+
+    // Using template 1
+    public class Solution extends VersionControl {
+        public int firstBadVersion(int n) {
+            int start = 1, end = n - 1;
+            while(start <= end) {
+                int mid = start + (end - start)/2;
+                if(isBadVersion(mid)) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+            return start;
+        }
+    }
